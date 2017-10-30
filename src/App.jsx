96d7d146a0dcc339de1fd2198991256ebe25e04d
Rachael Topp <rachaeltopp@Rachaels-MacBook-Air.local>
2017-10-30 18:35:56 -0400

    initial commit- page set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx'
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <body>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList />
+        <ChatBar />
+      </body>
     );
   }
 }
