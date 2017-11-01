3ae81021fee0cddc5ae7e1a9f2f406713d118b35
Rachael Topp <rachaeltopp@Rachaels-MacBook-Air.local>
2017-11-01 14:37:08 -0400

    set up web socket connection

--- src/App.jsx
+++ src/App.jsx
@@ -20,21 +20,15 @@ class App extends Component {
         }
       ]
     }
-    this.createMessage = this.createMessage.bind(this)
+    this.createMessage = this.createMessage.bind(this);
   }
 
-  // componentDidMount() {
-  //   console.log('componentDidMount <App />');
-  //   setTimeout(() => {
-  //     console.log('Simulating incoming message');
-  //     // Add a new message to the list of messages in the data store
-  //     const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
-  //     const messages = this.state.messages.concat(newMessage)
-  //     // Update the state of the app component.
-  //     // Calling setState will trigger a call to render() in App and all child components.
-  //     this.setState({messages: messages})
-  //   }, 3000);
-  // }
+  componentDidMount() {
+    var socket = new WebSocket('ws://localhost:3001');
+    socket.onopen = (event) => {
+      console.log("Connected to server");
+    }
+  }
 
   render() {
       return (
